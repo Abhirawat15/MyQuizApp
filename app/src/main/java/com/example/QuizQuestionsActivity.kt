613905b075554284b90874ce813768d7a8843ed8
Abhirawat15 <abhishekrawatvs158@gmail.com>
2023-08-19 00:33:03 +0530

    first commit

---
+++ app/src/main/java/com/example/QuizQuestionsActivity.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myquizapp.R
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+    }
+}
